f3f6a18d23a99ad1b90c6bc86aeb781f63fb1342
Slawomir Palka <rexsiomag@gmail.com>
2019-05-09 22:41:55 +0200

    Updates

---
+++ precache-manifest.325d6992a9df6bddbec9e2a34b56101d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca95fbbe31703daa3b7e357cffc9fd54",
+    "url": "/Szubienica/index.html"
+  },
+  {
+    "revision": "e073cd7d845a2d6ed76a",
+    "url": "/Szubienica/static/css/main.889d428b.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/Szubienica/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "e073cd7d845a2d6ed76a",
+    "url": "/Szubienica/static/js/main.2e21bd55.chunk.js"
+  },
+  {
+    "revision": "d76da9e88811fbbfdbe2",
+    "url": "/Szubienica/static/js/runtime~main.b76bbbd1.js"
+  }
+]);
